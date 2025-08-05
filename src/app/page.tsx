6f7e4c40ad03fbb8761241e8dc3e3b6635601f0e
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-05 04:12:59 +0100

    chore/set up homepage ... in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { CustomButton } from "@/components/button";
 import { DropDownPicker } from "@/components/drop_down_picker";
 import { Header } from "@/components/header";
 import Heading from "@/components/heading";
 import ProjectItem from "@/components/projectItem";
+import Title from "@/components/title";
 import Image from "next/image";
 import Link from "next/link";
 const links = [
@@ -21,20 +23,55 @@ export default function Home() {
   return (
     <div className="flex flex-col font-sans items-center justify-items-center min-h-screen">
       <Header headerContent={links} />
+      <section id="summary">
+        <ProjectItem
+          left={
+            <div className=" p-4 flex flex-col justify-evenly h-full ">
+              <div>
+                <Title text="Call me Jomo" className="text-left p-0" />
+                <p className="text-3xl text-left py-3">
+                  I'm a full stack developer specializing in building web
+                  applications using JavaScript, React and Node.js
+                </p>
+              </div>
+
+              <CustomButton>Find Out More</CustomButton>
+            </div>
+          }
+          right={
+            <div className=" p-4">
+              <img
+                src="https://andrejgajdos.com/wp-content/uploads/2021/05/andrej-gajdos-1.webp"
+                alt="Andrej Gajdos"
+                className="w-full max-h-[450px] rounded object-contain"
+              />
+            </div>
+          }
+        />
+      </section>
+      <section id="about">
+       
+          <Heading text="About" className="text-center"/>
+          <p className="text-xl text-left px:10 lg:px-20 ">
+            Discover my Freelance React Developer Portfolio, showcasing projects
+            for companies worldwide where I’ve built high-performance web
+            applications, interactive marketing websites, and dynamic data
+            visualizations. Specializing in React, Next.js, and Node.js, I focus
+            on creating seamless user experiences and robust, scalable
+            solutions.
+          </p>
+       
+      </section>
       <section
         id="projects"
         className="flex flex-col items-center justify-center text-center"
       >
         <Heading text={"Projects"} />
-        <p className="max-w-2xl text-left py-5">
-          Discover my Freelance React Developer Portfolio, showcasing projects
-          for companies worldwide where I’ve built high-performance web
-          applications, interactive marketing websites, and dynamic data
-          visualizations. Specializing in React, Next.js, and Node.js, I focus
-          on creating seamless user experiences and robust, scalable solutions.
-        </p>
 
-        <ProjectItem />
+        <ProjectItem
+          left={<div className="bg-red-400 p-4">Left Side Content</div>}
+          right={<div className="bg-amber-500 p-4">Right Side Content</div>}
+        />
       </section>
 
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start bg-gray-800">
