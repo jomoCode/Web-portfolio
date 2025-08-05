ec2e6e74426350aa7bd532b6fe02d1bf3dee5c5a
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-05 04:11:05 +0100

    feature/projectItem

--- src/components/projectItem.tsx
+++ src/components/projectItem.tsx
@@ -1,10 +1,16 @@
 import React, { ReactNode } from "react";
 
-const ProjectItem = ({left, right}:{left?:ReactNode; right?:ReactNode}) => {
+const ProjectItem = ({
+  left,
+  right,
+}: {
+  left?: ReactNode;
+  right?: ReactNode;
+}) => {
   return (
-    <div className="flex flex-row w-48 h-48">
-      <div className="bg-red-400 w-full  h-full">{left}</div>
-      <div className="bg-amber-500 w-full  h-full">{right}</div>
+    <div className="flex flex-col md:flex-row w-screen h-auto p-10 items-stretch">
+      <div className="w-full md:w-1/2 flex-1 overflow-auto">{left}</div>
+      <div className="w-full md:w-1/2 flex-1 overflow-auto">{right}</div>
     </div>
   );
 };
