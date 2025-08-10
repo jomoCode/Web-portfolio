20bf4058270fe6bd578dd09e9b2ddf2ec9c33941
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-11 00:47:48 +0100

    chore- updated custom button

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,23 +1,27 @@
+import { useTheme } from "@/context/theme_context";
+import { cva } from "class-variance-authority";
 import React from "react";
 
+const customButtonStyles = cva(
+  " flex items-center justify-center border px-4 py-4  transition-transform duration-300 ease-in-out hover:scale-80 shadow-md focus:outline-none focus:ring-2 focus:ring-blue-400",
+  {
+    variants: {
+      theme: {
+        dark: "bg-gray-800 text-white hover:text-gray-300 ",
+        light: "bg-white text-gray-900 hover:text-gray-500",
+      },
+    },
+    defaultVariants: {
+      theme: "dark",
+    },
+  }
+);
 const CustomButton = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
   const { children, className = "", ...rest } = props;
+  const { theme, toggleTheme } = useTheme();
 
   return (
-    <button
-      {...rest}
-      className={`
-        flex items-center justify-center
-        border border-gray-800
-        px-4 py-4
-        hover:bg-gray-100
-        active:scale-95
-        transition-transform duration-200
-        shadow-md
-        focus:outline-none focus:ring-2 focus:ring-blue-400
-        ${className}
-      `}
-    >
+    <button {...rest} className={customButtonStyles({ theme, className })}>
       {children}
     </button>
   );
