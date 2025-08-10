9a83650185bef606afb153aa3d83f28bb28d2a3e
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-10 13:17:48 +0100

    update - header to use headerContainer and theme context

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,17 +1,37 @@
-import { ReactNode } from "react";
+import { ReactNode, use } from "react";
 import { Bars4Icon } from "@heroicons/react/24/solid";
+import { cva } from "class-variance-authority";
+import { HeaderContainer } from "./header/header_container";
+import { useTheme } from "@/context/theme_context";
+
+export const headerVariants = cva(
+  "w-[7.5rem] py-2 text-lg   text-center hover:text-xl  transition-all duration-300 ease-in-out",
+  {
+    variants: {
+      theme: {
+        light: "bg-white text-slate-800 hover:text-gray-500 ",
+        dark: "text-white hover:text-slate-400",
+      },
+    },
+    defaultVariants: {
+      theme: "dark",
+    },
+  }
+);
 
 export const Header = ({
   headerContent,
 }: {
   headerContent: (string | ReactNode)[];
-}) => (
-  <header className="w-full border-b-2 border-slate-700 dark:border-slate-300 flex items-center justify-between md:justify-center px-4 md:px-0 py-4 md:py-5 shadow-md dark:shadow-slate-900 bg-white dark:bg-gray-900">
+}) => {
+  const {theme, toggleTheme} = useTheme();
+  return(
+  <HeaderContainer>
     <ul className="hidden md:flex md:flex-row gap-4">
       {headerContent.map((data, index) => (
         <li
           key={index}
-          className="w-[7.5rem] py-4 text-lg text-slate-800 dark:text-slate-200 text-center hover:text-xl hover:text-gray-500 dark:hover:text-slate-400 transition-all duration-300 ease-in-out"
+          className={headerVariants({ theme })}
         >
           {data}
         </li>
@@ -20,7 +40,9 @@ export const Header = ({
 
     <div className="flex items-center gap-2 md:hidden">
       <Bars4Icon className="h-6 w-6 text-gray-800 dark:text-white" />
-      <p className="text-xl font-bold text-gray-900 dark:text-white">JomoCode</p>
+      <p className="text-xl font-bold text-gray-900 dark:text-white">
+        JomoCode
+      </p>
     </div>
-  </header>
-);
+  </HeaderContainer>
+)};
