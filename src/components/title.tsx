9c52934ddfff12daea695cff7c649357715b25a7
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-12 11:06:21 +0100

    feature and chore -new feat: text. added theme context to text feats

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,17 +1,35 @@
-import React from "react";
+import { useTheme } from "@/context/theme_context";
+import React, { ReactNode } from "react";
 
 interface TitleProps extends React.HTMLAttributes<HTMLHeadingElement> {
   text: string;
 }
 
 const Title = ({ text, className = "", ...rest }: TitleProps) => {
+  const {theme, toggleTheme} = useTheme();
   return (
     <div>
-      <h1 className={`text-xl ${className}`} {...rest}>
+      <h1 className={`text-xl ${className}, ${theme === "dark"? "text-text_dark":"text-text-light" }`} {...rest}>
         {text}
       </h1>
     </div>
   );
 };
 
-export default Title;
+interface TextProps extends React.HTMLAttributes<HTMLParagraphElement> {
+  text?: string|ReactNode;
+  childen?: string|ReactNode;
+}
+
+const Text = ({ text, children, className = "", ...rest }: TextProps) => {
+ const {theme, toggleTheme} = useTheme();
+  return (
+    <div>
+      <p className={`text-xl ${className}   ${theme === "dark"? "text-text_dark":"text-text-light" }`} {...rest}>
+        {children}
+      </p>
+    </div>
+  );
+};
+
+export { Text, Title };
