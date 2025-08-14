717041cabe004697c35511f50ca22fe570bb0b8c
jomoartz <johnjohananmogbolu@gmail.com>
2025-08-14 08:42:11 +0100

    (sideByside)- fix all screens alignment

--- src/components/side_by_side_section.tsx
+++ src/components/side_by_side_section.tsx
@@ -8,7 +8,7 @@ const  SideBySideSection= ({
   right?: ReactNode;
 }) => {
   return (
-    <div className="flex flex-col md:flex-row w-screen h-auto px-10 py-0 items-stretch">
+    <div className="flex flex-col md:flex-row w-full h-auto gap-5 justify-center items-center mb-10">
       <div className="w-full md:w-1/2 flex-1 overflow-auto">{left}</div>
       <div className="w-full md:w-1/2 flex-1 overflow-auto">{right}</div>
     </div>
